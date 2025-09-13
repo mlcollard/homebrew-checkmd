1b4ad141903037722dbe3f6b5c48ff919260e83c
Michael L. Collard <collard@uakron.edu>
2025-09-13 05:45:54 -0400

    Fix url

--- checkmd.rb
+++ checkmd.rb
@@ -1,12 +1,12 @@
 class Checkmd < Formula
   desc "Markdown file checker and validator"
   homepage "https://github.com/mlcollard/checkmd"
-  url "https://github.com/mlcollard/checkmd/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d3b7022d72cbd09951602dce4633c8e150d3685dcd232a3a294997ceff6db4e9"
+  url "https://github.com/mlcollard/homebrew-checkmd/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "b1f504453f28010d67ab0d93ba7f721e73e8afa83333d99d810ac017c5050d5b"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d3b7022d72cbd09951602dce4633c8e150d3685dcd232a3a294997ceff6db4e9"
+    sha256 cellar: :any_skip_relocation, all: "b1f504453f28010d67ab0d93ba7f721e73e8afa83333d99d810ac017c5050d5b"
   end
 
   def install
